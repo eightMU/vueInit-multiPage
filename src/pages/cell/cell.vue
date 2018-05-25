fed67bea9ac578c4b84a1379a67e5b574ea1c9a4
yhy <405939579@qq.com>
2018-05-25 14:15:08 +0800

    '初始化页面'

---
+++ src/pages/cell/cell.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!--<img src="../../assets/logo.png">-->
+    <div>nihao</div>
+    <a href="index.html">nihao</a>
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
